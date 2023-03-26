4f09199cfa639e93478208130144098a0e3eeaaf
tekletsadik21 <tekletsadikethiopia@gmail.com>
2023-03-26 13:28:16 +0300

    some bug fix

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL:"https://api.unsplash.com/",
     headers:{
-        Authorization: "Client-ID S-7JYOo60tnyvzbStfRZs9Xgz84wLosCGc0FReZApcw"
+        Authorization: "Client-ID "
     }
 });
